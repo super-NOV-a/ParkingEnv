7c9611ea926e37a07fea70765c93da00542004d4
Liu Yiyang <70046229+super-NOV-a@users.noreply.github.com>
2025-07-11 16:09:01 +0800

    Delete env2 directory

--- env2/parking_env_pkg/__init__.py
+++
@@ -1,4 +0,0 @@
-# parking_env_pkg/__init__.py
-from .parking_core import ParkingEnv
-
-__all__ = ["ParkingEnv"]
--- env2/register.py
+++
@@ -1,9 +0,0 @@
-from parking_env2 import ParkingEnvArc          # 已写好的
-from parking_env import ParkingEnv              # 原始（连续动作）
-# from parking_env_disc import ParkingEnvDiscrete # 如果已有离散版
-
-ENV_REGISTRY = {
-    "arc":       ParkingEnvArc,
-    "orig":      ParkingEnv,          # 连续 Box 动作
-    # "orig_disc": ParkingEnvDiscrete,  # MultiDiscrete 动作
-}
